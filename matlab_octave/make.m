df2b8dd74e208cdd4104a64bc030c1a4a8abae11
drdv <mail.mitko@gmail.com>
2017-07-20 00:46:10 +0200

    Makefile: install mex files by defualt. Include note about using eigen

--- matlab_octave/make.m
+++ matlab_octave/make.m
@@ -6,4 +6,7 @@ copyfile('config.hh', '../src');
 mex -v -I../src/   quadprogpp_interface.cpp  ../src/Array.cc ../src/QuadProg++.cc
 %mex -v -I../src/ -I/usr/local/include/eigen3/  quadprogpp_interface.cpp  ../src/Array.cc ../src/QuadProg++.cc
 
+% note: when I define QUADPROGPP_ENABLE_EIGEN, I get unexpected results
+%       on the test qpgi/matlab/other_test_problems/test_problem_005.m
+
 %%%EOF
